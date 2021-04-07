b00ea92c81c83f8763036bac6cc9865203fbb391
Ivo Gsellmann <ivo.gsellmann@gmx.at>
2021-04-07 18:42:55 +0200

    initial commit

---
+++ calculator.py
@@ -0,0 +1,21 @@
+print("Welcome to the calculator - lets do some calculations!")
+
+zahl1 = int(input("Please insert your first number: "))
+zahl2 = int(input("Please insert your second number: "))
+rechenoperation = input("Please insert your arithmetic operation [+ | - | * | / ]: ")
+
+if rechenoperation == "+":
+    ergebnis = zahl1 + zahl2
+elif rechenoperation == "-":
+    ergebnis = zahl1 - zahl2
+elif rechenoperation == "*":
+    ergebnis = zahl1 * zahl2
+elif rechenoperation == "/":
+    if zahl2 == 0:
+        print("Division durch Null nicht möglich!")
+        ergebnis = "undefined"
+    else:
+        ergebnis = zahl1 / zahl2
+
+print("The arithmetic operation chosen was: " + str(rechenoperation))
+print("The result is: " + str(ergebnis))
